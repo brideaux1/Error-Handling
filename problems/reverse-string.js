2d1cdd3089ddaf12777d5110f04db1f04fd03523
Britney <britney.rideaux@yahoo.com>
2023-04-06 00:09:57 -0500

    reverse string

--- problems/reverse-string.js
+++ problems/reverse-string.js
@@ -1,3 +1,8 @@
 module.exports = function reverseString(string) {
+  if (typeof string !== "string") {
+    throw new TypeError("Input must be a string");
+  }
+
+  return string.split("").reverse().join("");
   // Your code here
 };
--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,9 @@
-// Your code here
+const chai = require("chai");
+const expect = chai.expect;
+const reverseString = require("../problems/reverse-string");
+
+describe("reverseString", function () {
+  it("should reverse the string 'fun'", function () {
+    expect(reverseString("fun")).to.equal("nuf");
+  });
+});
